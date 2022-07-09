65b34441357d9075ca5580543e8f72160bf6da94
Sandeep Singh Samant <travelblues@Sandeeps-MacBook-Pro.local>
2022-07-09 12:25:31 +0200

    FIle added

---
+++ linux64/src/cpp/Expr.h
@@ -0,0 +1,13 @@
+// Copyright (C) 2022, Gurobi Optimization, LLC
+// All Rights Reserved
+#ifndef _CPP_EXPR_H_
+#define _CPP_EXPR_H_
+
+class GRBExpr
+{
+  private:
+
+  public:
+    friend class GRBModel;
+};
+#endif
